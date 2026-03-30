1b97b6563b5d94c96fb71853546ec16a2adc6c5b
Jubrf <brefie.julien@gmail.com>
2026-03-30 12:12:54 +0200

    Update ndvi_processing.py

--- utils/ndvi_processing.py
+++ utils/ndvi_processing.py
@@ -1,19 +1,11 @@
 import ee
+import numpy as np
 
 def zonal_stats_ndvi(ndvi_img, veg_mask, geom):
-    """
-    Calcule :
-    - NDVI moyen de la parcelle
-    - proportion NDVI > seuil (uniquement si veg_mask != None)
-    Fonction compatible Analyse simple et Comparaison NDVI.
-    """
 
-    # Convertir géométrie shapely → Earth Engine
     geom_ee = ee.Geometry.Polygon(list(geom.exterior.coords))
 
-    # -------------------------------------------------------
-    # ✅ NDVI MOYEN (toujours calculé)
-    # -------------------------------------------------------
+    # mean NDVI
     mean_dict = ndvi_img.reduceRegion(
         reducer=ee.Reducer.mean(),
         geometry=geom_ee,
@@ -21,19 +13,6 @@ def zonal_stats_ndvi(ndvi_img, veg_mask, geom):
         maxPixels=1e9
     ).getInfo()
 
-    ndvi_mean = mean_dict.get("NDVI", None)
-    if ndvi_mean is not None:
-        ndvi_mean = float(ndvi_mean)
-
-    # -------------------------------------------------------
-    # ✅ MODE COMPARATEUR : veg_mask = None
-    # -------------------------------------------------------
-    if veg_mask is None:
-        return ndvi_mean, None
-
-    # -------------------------------------------------------
-    # ✅ MODE ANALYSE SIMPLE : calcul proportion NDVI > 0.25
-    # -------------------------------------------------------
     veg_dict = veg_mask.reduceRegion(
         reducer=ee.Reducer.mean(),
         geometry=geom_ee,
@@ -41,8 +20,12 @@ def zonal_stats_ndvi(ndvi_img, veg_mask, geom):
         maxPixels=1e9
     ).getInfo()
 
-    veg_prop = veg_dict.get("VEG", None)
-    if veg_prop is not None:
-        veg_prop = float(veg_prop)
+    ndvi = mean_dict.get("NDVI", None)
+    veg = veg_dict.get("VEG", None)
 
-    return ndvi_mean, veg_prop
+    if ndvi is not None:
+        ndvi = float(ndvi)
+    if veg is not None:
+        veg = float(veg)
+
+    return ndvi, veg
